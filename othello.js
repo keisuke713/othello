41a507d8ca8c1ee75bdb0ef479dd283ea5f71de3
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2021-07-18 11:58:58 +0900

    スケルトンメソッド作成

--- othello.js
+++ othello.js
@@ -1,11 +1,44 @@
 class Facilitator{
     constructor(users, board){
-        this.users = users;
-        this.board = board;
+        this.users              = users;
+        this.board              = board;
+        this.currentUserIndex   = 0;
+        this.amountOfBlackStone = 0;
+        this.amountOfWhiteStone = 0;
     }
 
-    run(){}
-    ifUserCanPutStoneOnTheCell(user, cell){}
+    // 既に石が置かれているかどうか
+    ifAStoneIsOnTheCell(row, col){
+        this.board.ifAStoneIsOnTheCell(row, col);
+    }
+    // ユーザーが石を置けるかどうか
+    ifUserCanPutStoneOnTheCell(row, col){
+        this.currentUser().canPutAStoneOnTheCell(this.board, row, col);
+    }
+    // ユーザーが石をおく
+    userPutAStoneOnTheCell(row, col){
+        this.currentUser().putAStone(this.board, row, col);
+    }
+    // 石をひっくり返す
+    userReverseStones(row, col){
+        this.currentUser().reverseStones(this.board, row, col);
+    }
+    // currentUserを更新
+    currentUser(){
+        return this.users[this.currentUserIndex];
+    }
+    // インデックスを更新
+    updateIndex(){
+        this.currentUserIndex = (this.currentUserIndex + 1) & 1;
+    }
+    // 盤の黒の石の数を更新
+    setAmountOfBlackStone(){
+        this.amountOfBlackStone = this.board.getAmountOfStones("black");
+    }
+    // 盤の白の石の数を更新
+    setAmountOfWhiteStone(){
+        this.amountOfWhiteStone =this.board.getAmountOfStones("white");
+    }
 }
 
 class User{
@@ -15,9 +48,13 @@ class User{
         this.stoneColor = stoneColor;
     }
 
-    putAStone(cell){}
-    reverseStone(board, cell){}
-    getCellsUserCanPutAStone(board){}
+    // 該当のセルに石を置けるかどうか
+    canPutAStoneOnTheCell(board, row, col){}
+    // ユーザーが石をおく
+    putAStone(board, row, col){}
+    // 石をひっくり返す
+    reverseStones(board, row, col){}
+    // getCellsUserCanPutAStone(board){}
 }
 
 class Board{
@@ -25,7 +62,11 @@ class Board{
         this.cells = cells;
     }
 
-
+    // 既に石が置かれているかどうか
+    ifAStoneIsOnTheCell(row, col){
+        cells[row][col].isStoneOn();
+    }
+    getAmountOfStones(color){}
     isThereAnyCellUserCanPutAStone(user){}
     isAllCellPutAStone(){}
 }
@@ -37,16 +78,32 @@ class Cell{
         this.col   = col;
     }
 
-    isPutAStone(){}
+    // 既に石が置かれているかどうか
+    isStoneOn(){}
+    isStoneBlack(){}
+    isStoneWhite(){}
 }
 
 class Stone{
     constructor(color){
-        this.color = color;
+        this.color      = color;
+        this.image = "";
     }
 
-    isBlack(){}
-    isWhite(){}
+    isBlack(){
+        return this.color = "black";
+    }
+    isWhite(){
+        return this.color == "white";
+    }
+    turnIntoBlack(){
+        this.color = "black";
+        this.image = "black.png";
+    }
+    turnIntoWhite(){
+        this.color = "white";
+        this.image = "white.png";
+    }
 }
 
 class UserBuilder{}
